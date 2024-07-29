a11d3aeca23d6e9376267260f669b09717312317
GitHub Action <action@github.com>
2024-07-29 17:07:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/zcd0831/myideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/zcd0831/myideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zcd0831.myideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zcd0831/myideaplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/zcd0831/myideaplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.zcd0831.myideaplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
